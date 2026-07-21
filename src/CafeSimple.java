875db79ac414be52eb6e8984cc871cc2b536ee76
Sebastian Salazar Aguilar <salazaraguilarsebastianjose@gmail.com>
2026-07-21 03:41:26 -0500

    feat: se creo la clase CafeSimple implementada de la iterfaz Bebida

---
+++ src/CafeSimple.java
@@ -0,0 +1,11 @@
+public class CafeSimple implements Bebida {
+    @Override
+    public String getDescripcion() {
+        return "Cafe Simple";
+    }
+
+    @Override
+    public double getCosto() {
+        return 5.00;
+    }
+}
